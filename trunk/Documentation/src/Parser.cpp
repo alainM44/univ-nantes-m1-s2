dccca456555fc044e23b788bdbe812fef80664b8
romainrince <romainrince@4319bafd-bb82-3f64-fc94-89c43f1aa53d>
2012-01-11 16:30:09 +0000

    Version 1 du parser Possibilité de problème de gestion de la mémoire et des copies

--- trunk/Documentation/src/Parser.cpp
+++ trunk/Documentation/src/Parser.cpp
@@ -2,52 +2,55 @@
 
 using namespace std;
 
-
-
-vector<string> Parser::Split_meta(const string &ligne, char separator) const
-{
-	int pos=0;
-	vector <string> result;
-	pos=ligne.find(separator);
-	result.push_back(ligne.substr(1,pos-1)) ;
-	result.push_back(ligne.substr(pos+1,ligne.size()-pos+1));
+vector<string> Parser::Split_meta(const string &ligne) const {
+	int pos = 0;
+	vector<string> result;
+	pos = ligne.find(':');
+	result.push_back(ligne.substr(1, pos - 1));
+	result.push_back(ligne.substr(pos + 1, ligne.size() - pos + 1));
 	return result;
-} 
+}
 
-vector<string> Parser::Split_do(const string &ligne) const
-{
-	//sdfdsfdsf
-	int debut_donnee=0;
+vector<string> Parser::Split_do(const string & ligne) const {
+	int debut_donnee = 0;
 	int fin_donnee;
-	vector <string> result; 
-	// Parcours de la ligne en stockant les élémant et en "sautant" les espaces
-	while(ligne.find(ligne,separator))
-	{
-		fin_donnee=ligne.find(separator);
-		result.push_back(ligne.substr(debut_donnee,fin_donnee-debut_donne));
-		debut_donnee=fin_donnee;
-		while(ligne.at(debut_donnee)== " ")
-		{
+	vector<string> result;
+	// Parcours de la ligne en stockant les éléments et en "sautant" les espaces
+	while (ligne.find(' ')) {
+		fin_donnee = ligne.find(' ');
+		result.push_back(ligne.substr(debut_donnee, fin_donnee - debut_donnee));
+		debut_donnee = fin_donnee;
+		while ((ligne.at(debut_donnee)) == ' ') {
 			debut_donnee++;
 		}
 	}
 	return result;
-} 
+}
 
+void Parser::parse(vector<vector<string> >& meta,
+		vector<vector<string> >& donnees) const {
+	//conversion de la string en char* pour permettre la création du ifstream.
+	ifstream fichier(m_nom_fichier.c_str());
+	string ligne;
+	if (fichier) {
+		while (getline(fichier, ligne)) {
+			if (ligne.at(0) == '>') {
+				meta.push_back(Split_meta(ligne));
+			}
+			else
+			{
+				donnees.push_back(Split_do(ligne));
+			}
+		}
+	}
+}
 
-
-Parser::Parser(string nom_fichier) : m_nom_fichier(nom_fichier)
-{
+Parser::Parser(const string nom_fichier) :
+	m_nom_fichier(nom_fichier) {
 
 }
 
-Parser::~Parser()
-{
+Parser::~Parser() {
 	//dtor
 }
-
-
-
-
-
 
--- trunk/Documentation/src/Parser.h
+++ trunk/Documentation/src/Parser.h
@@ -1,5 +1,6 @@
 #ifndef PARSER_H_
 #define PARSER_H_
+#include <fstream>
 #include <vector>
 #include <string>
 
@@ -8,33 +9,48 @@
  *
  *  La classe rentre les données dans des tableaux de types vector
  */
-class Parser
-{
+class Parser {
 
-public :
-	
-	/**
-	 * 
-	 * @param ligne a parser
+public:
+
+	/*!
+	 * \brief décompose une ligne de Métadonnées pour retourner un tableau de 2 cases
+	 * \brief contenant le nom de la métadonnée dans la première case et
+	 * \brief le contenu dans le second
+	 * @param ligne ligne de métadonnée a parser
 	 * @return tableau remplit avec les meta données
 	 * 
 	 */
-	std::vector<std::string> Split_meta(const std::string & ligne, char separator) const;
-	/**
-	 * @param ligne a parser
+	std::vector<std::string> Split_meta(const std::string & ligne) const;
+
+	/*!
+	 *\brief decompose une ligne de données du tableau et retourne dans un vecteur les
+	 *\brief valeurs extraites
+	 * @param ligne ligne a parser
 	 * @return tableau remplit avec les meta données
 	 */
 	std::vector<std::string> Split_do(const std::string &ligne) const;
-	/**
+
+	/*!
+	 *\brief L'utilisateur fourni deux tableaux vides (de préférence) qui seront remplis respectivement
+	 *\brief par les métadonnées et les données du fichier.
+	 *@param meta Contient les métadonnées du fichier en sortie de la méthode.
+	 *@param donnees Contient les données du fichier en sortie de la méthode.
+	 */
+	void parse(std::vector<std::vector<std::string> >& meta, std::vector<
+			std::vector<std::string> >& donnees) const;
+
+	/*!
 	 * destructeur
 	 */
 	~Parser();
-	/**
+
+	/*!
 	 * Constructeur
 	 */
-	Parser(std::string nom_fichier);
-	
-private: 
+	Parser(const std::string nom_fichier);
+
+private:
 	std::string m_nom_fichier;
 
 };
