bc54f12e06aa4a1e519d8806ef0a0e015e634966
joe.bizet <joe.bizet@4319bafd-bb82-3f64-fc94-89c43f1aa53d>
2012-03-25 17:37:30 +0000

    Branche Middleware-[<-Reasoning OKKKK!!

---
+++ trunk/Analyse/src/Analyse.java
@@ -0,0 +1,15 @@
+import java.awt.image.BufferedImage;
+import reasoning.IAnalyse;
+
+
+public class Analyse implements IAnalyse {
+
+	public Analyse() {
+	}
+	@Override
+	public Object makeAnalyse(BufferedImage bi) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
